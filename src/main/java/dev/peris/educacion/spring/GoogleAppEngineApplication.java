f453e1549c1f74ddd2a80eaac8b8fd6cbc4fa1f0
a508571 <a508571@MC0XHTMC.ww930.my-it-solutions.net>
2019-04-24 11:08:24 +0200

    add gae

---
+++ src/main/java/dev/peris/educacion/spring/GoogleAppEngineApplication.java
@@ -0,0 +1,13 @@
+package dev.peris.educacion.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAppEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAppEngineApplication.class, args);
+	}
+
+}
